db72982c5dcd2c6d3b5a7d4753cf8fc110e78792
Mayur <mayur.pathak52@gmail.com>
2017-09-12 22:46:22 +0530

    typo fixed

--- resources/views/email/template.blade.php
+++ resources/views/email/template.blade.php
@@ -22,7 +22,7 @@
                   <div class="name" style="margin: 2% auto 0% auto; font-size: 1.25em; text-align: center;">Hello <b>{{$name}}</b> </div>
                   <div class="thanking" style="font-size:1.5em; color: #FED766; margin: 2% auto; text-align: center;">Thank you for Registration</div>
                   <div class="text" style="font-size:1em; text-align: center; line-height: 1.25em; padding:0 4%;">Your team has been successfully registered for SCROLLS 2017. Your Team ID is {{$team_id}}. Soon you will be able to login with your Team ID and password for uploading synopsis.</div> 
-                  <div class="website_link" style="font-size:9em; text-align: center;margin-top: 1%;">Stay tuned with Us <a style="color:#FED766;">www.akgec-scrolls.com</a></div> 
+                  <div class="website_link" style="font-size:1em; text-align: center;margin-top: 1%;">Stay tuned with Us <a style="color:#FED766;">www.akgec-scrolls.com</a></div> 
                   <div class="footer" style="font-size: 1em; margin: 1% 21%; background-color: #FED766; text-align: center;padding:1%;">Powered by<span style="color:#739bd1"> Software Incubator</span> </div>
               </div>
           </div>
